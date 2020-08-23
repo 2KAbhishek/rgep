71eef54db60443be9ecff76f51397f0155c6ea40
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-23 12:52:45 +0530

    Add Command line arguments with structopt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use structopt::StructOpt;
+
+// Search fro pattern in path
+#[derive(StructOpt)]
+struct Cla {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cla::from_args();
+    println!("{} {:?}", args.pattern, args.path);
 }
