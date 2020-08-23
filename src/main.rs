6ae14aeab5d5c9c346134a1235519d6968e174a3
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-23 13:46:04 +0530

    Use BufReader for reading lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 use structopt::StructOpt;
 
 // Search fro pattern in path
@@ -11,11 +14,21 @@ struct Cla {
 fn main() {
     let args = Cla::from_args();
 
-    let content = std::fs::read_to_string(&args.path).expect("Could not read file.");
-
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
+    if let Ok(lines) = read_lines(args.path) {
+        for line in lines {
+            if let Ok(line) = line {
+                if line.contains(&args.pattern) {
+                    println!("{}", line);
+                }
+            }
         }
     }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
